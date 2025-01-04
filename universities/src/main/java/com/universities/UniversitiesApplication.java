eae8f22c31db0061f539454433e96151a9426b35
Gopal Raju <dommarajugopal6@gmail.com>
2025-01-04 20:54:49 +0530

    Initial base commit

---
+++ universities/src/main/java/com/universities/UniversitiesApplication.java
@@ -0,0 +1,13 @@
+package com.universities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitiesApplication.class, args);
+	}
+
+}
